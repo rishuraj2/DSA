6d45a43f36317b505bdf177bc6903497345648fb
Rishu Raj <sharmarishu695@gmail.com>
2025-12-15 23:30:57 +0530

    added solution for searchRange

---
+++ src/searching/SearchRange.java
@@ -0,0 +1,50 @@
+package searching;
+
+public class SearchRange {
+    public static int[] searchRange(int[] nums, int target) {
+        int[] result = new int[]{-1, -1};
+        int lastIndex = nums.length - 1;
+
+        int low = 0;
+        int high = lastIndex;
+
+        while (low <= high){
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    result[0] = mid;
+                    break;
+                } else {
+                    high = mid - 1;
+                }
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        low = 0;
+        high = lastIndex;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                if (mid == lastIndex || nums[mid + 1] != target) {
+                    result[1] = mid;
+                    break;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return result;
+    }
+}
