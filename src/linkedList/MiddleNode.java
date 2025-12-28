39278eaf3773c8c6934a72069bc88d5008f8fdf0
Rishu Raj <sharmarishu695@gmail.com>
2025-12-28 23:39:29 +0530

    added solution for MiddleNode

---
+++ src/linkedList/MiddleNode.java
@@ -0,0 +1,15 @@
+package linkedList;
+
+public class MiddleNode {
+    public ListNode middleNode(ListNode head) {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+
+        return slowPointer;
+    }
+}
