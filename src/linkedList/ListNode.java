b34555ab27d515ee23bfbfdb4e2284362da2d7a6
Rishu Raj <sharmarishu695@gmail.com>
2025-12-28 22:28:05 +0530

    added solution for DeleteNode

---
+++ src/linkedList/ListNode.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int value, ListNode next) {
+        this.val = value;
+        this.next = next;
+    }
+}
