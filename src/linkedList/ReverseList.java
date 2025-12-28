c2c6da2d1326a96c70407b9c51f53674a4a2d13f
Rishu Raj <sharmarishu695@gmail.com>
2025-12-28 23:40:54 +0530

    added solution for reverseList

---
+++ src/linkedList/ReverseList.java
@@ -0,0 +1,27 @@
+package linkedList;
+
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode temp1 = head.next;
+        ListNode temp2 = temp1.next;
+        head.next = null;
+        while (temp1 != null) {
+            temp1.next = head;
+            head = temp1;
+            temp1 = temp2;
+            if (temp2 != null) {
+                temp2 = temp2.next;
+            }
+        }
+        return head;
+    }
+}
+}
