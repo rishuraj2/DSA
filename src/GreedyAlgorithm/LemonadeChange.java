710dea4ff24b553f5d56493889e8244939308d6d
Rishu Raj <sharmarishu695@gmail.com>
2025-12-30 01:43:01 +0530

    added solution for lemonadeChange

---
+++ src/GreedyAlgorithm/LemonadeChange.java
@@ -0,0 +1,31 @@
+package GreedyAlgorithm;
+
+public class LemonadeChange {
+    public static boolean lemonadeChange(int[] bills) {
+        int fiveCount = 0;
+        int tenCount = 0;
+
+        for (int bill : bills) {
+            switch(bill) {
+                case 5 -> fiveCount++;
+                case 10 -> {
+                    if (fiveCount == 0)
+                        return false;
+                    fiveCount--;
+                    tenCount++;
+                }
+                case 20 -> {
+                    if (tenCount != 0 && fiveCount != 0) {
+                        tenCount--;
+                        fiveCount--;
+                    } else if (fiveCount >= 3) {
+                        fiveCount -= 3;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
