14dbe5a7f4fbb861cc9834e87e14889714c1ff27
Rishu Raj <sharmarishu695@gmail.com>
2025-12-30 15:43:50 +0530

    added solution for RemoveDuplicates

---
+++ src/Arrays/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class RemoveDuplicates {
+    public static int removeDuplicates(int[] nums) {
+        int replaceIndex = 0;
+        int searchIndex = 0;
+        
+        while (replaceIndex < nums.length-1 && searchIndex < nums.length) {
+            if (nums[replaceIndex] == nums[searchIndex]) {
+                searchIndex++;
+            } else {
+                nums[replaceIndex+1] = nums[searchIndex];
+                replaceIndex++;
+            }
+        }
+
+        return replaceIndex+1;
+    }
+}
